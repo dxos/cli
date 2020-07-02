3f5b5a7af2058494df179f9d4e8dae66782a0f1e
egorgripasov <gripasovegor@gmail.com>
2020-07-02 16:59:45 -0400

    cli-app to update served config

--- packages/cli-app/src/handlers/index.js
+++ packages/cli-app/src/handlers/index.js
@@ -6,4 +6,5 @@ export * from './build';
 export * from './publish';
 export * from './register';
 export * from './query';
+export { default as configuration } from './config';
 export { default as serve } from './serve';
