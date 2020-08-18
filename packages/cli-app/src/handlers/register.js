0046cdf45431557496920e382fb70605445b2f30
egorgripasov <gripasovegor@gmail.com>
2020-08-18 17:17:25 -0400

    cli-app changes.

--- packages/cli-app/src/handlers/register.js
+++ packages/cli-app/src/handlers/register.js
@@ -13,7 +13,7 @@ import { Registry } from '@wirelineio/registry-client';
 import { APP_CONFIG_FILENAME } from '../config';
 
 export const register = (config, { getAppRecord }) => async (argv) => {
-  const { verbose, name, id, namespace, 'dry-run': noop, txKey } = argv;
+  const { verbose, version, namespace, 'dry-run': noop, txKey } = argv;
   const wnsConfig = config.get('services.wns');
   const { server, userKey, bondId, chainId } = wnsConfig;
 
@@ -22,11 +22,14 @@ export const register = (config, { getAppRecord }) => async (argv) => {
   assert(bondId, 'Invalid WNS Bond ID.');
   assert(chainId, 'Invalid WNS Chain ID.');
 
-  const appConfig = await readFile(APP_CONFIG_FILENAME);
+  const { names = [], ...appConfig } = await readFile(APP_CONFIG_FILENAME);
+  const { name = names } = argv;
+
+  assert(Array.isArray(name), 'Invalid App Record Name.');
 
   const conf = {
     ...appConfig,
-    ...clean({ id, name })
+    ...clean({ version })
   };
 
   log(`Registering ${conf.name}@${conf.version}...`);
@@ -42,17 +45,24 @@ export const register = (config, { getAppRecord }) => async (argv) => {
     log(JSON.stringify({ registry: server, namespace, record }, undefined, 2));
   }
 
-  if (noop) {
-    return;
-  }
-
-  if (!isEqual(conf, appConfig)) {
-    await writeFile(conf, APP_CONFIG_FILENAME);
-  }
-
   const fee = getGasAndFees(argv, wnsConfig);
-  const response = await registry.setRecord(userKey, record, txKey, bondId, fee);
-  console.log(response.data);
+
+  let appId;
+  if (!noop) {
+    if (!isEqual(conf, appConfig)) {
+      await writeFile(conf, APP_CONFIG_FILENAME);
+    }
+    const result = await registry.setRecord(userKey, record, txKey, bondId, fee);
+    appId = result.data;
+    log(`Record ID: ${appId}`);
+  }
+
+  for await (const wrn of name) {
+    log(`Assigning name ${wrn}...`);
+    if (!noop) {
+      await registry.setName(wrn, appId, userKey, fee);
+    }
+  }
 
   log(`Registered ${conf.name}@${conf.version}.`);
 };
