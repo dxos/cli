e37bcfa8bc03127264df5d29328befe986926ec5
Ashwin Phatak <ashwinpphatak@gmail.com>
2020-11-19 21:36:16 +0530

    Wallet and channel commands.

--- packages/cli-payment/src/modules/payment.js
+++ packages/cli-payment/src/modules/payment.js
@@ -2,6 +2,8 @@
 // Copyright 2020 DXOS.org
 //
 
+import assert from 'assert';
+
 import { asyncHandler } from '@dxos/cli-core';
 import { log } from '@dxos/debug';
 
@@ -11,6 +13,56 @@ export const PaymentModule = ({ paymentManager }) => ({
   builder: yargs => yargs
     .option('server', { type: 'string' })
     .option('interactive', { hidden: true, default: true })
+
+    .command({
+      command: ['wallet'],
+      describe: 'Wallet operations.',
+      builder: yargs => yargs
+        .option('interactive', { hidden: true, default: true })
+
+        .command({
+          command: ['address'],
+          describe: 'Get wallet address.',
+          builder: yargs => yargs
+            .option('interactive', { hidden: true, default: true }),
+
+          handler: asyncHandler(async () => {
+            const address = await paymentManager.getWalletAddress();
+            log(JSON.stringify({ address }, undefined, 2));
+          })
+        })
+
+        .command({
+          command: ['balance'],
+          describe: 'Get wallet balance.',
+          builder: yargs => yargs
+            .option('interactive', { hidden: true, default: true }),
+
+          handler: asyncHandler(async () => {
+            const balance = await paymentManager.getWalletBalance();
+            log(JSON.stringify({ balance }, undefined, 2));
+          })
+        })
+
+        .command({
+          command: ['send [address] [amount]'],
+          describe: 'Send funds using wallet.',
+          builder: yargs => yargs
+            .option('interactive', { hidden: true, default: true })
+            .option('address', { type: 'string' })
+            .option('amount', { type: 'string' }),
+
+          handler: asyncHandler(async (argv) => {
+            const { address, amount } = argv;
+
+            assert(address, 'Invalid address.');
+            assert(amount, 'Invalid amount.');
+
+            const txReceipt = await paymentManager.sendFunds(address, amount);
+            log(JSON.stringify(txReceipt, undefined, 2));
+          })
+        })
+    })
 
     .command({
       command: ['server'],
@@ -23,21 +75,22 @@ export const PaymentModule = ({ paymentManager }) => ({
             .option('interactive', { hidden: true, default: true }),
 
           handler: asyncHandler(async () => {
-            const info = await paymentManager.getInfo();
+            const info = await paymentManager.getId();
             log(JSON.stringify(info, undefined, 2));
           })
         })
-    })
 
-    .command({
-      command: ['wallet'],
-      describe: 'Wallet operations.',
-      builder: yargs => yargs
-        .option('interactive', { hidden: true, default: true }),
+        .command({
+          command: ['info'],
+          describe: 'Payment channel server info.',
+          builder: yargs => yargs
+            .option('interactive', { hidden: true, default: true }),
 
-      handler: asyncHandler(async () => {
-
-      })
+          handler: asyncHandler(async () => {
+            const info = await paymentManager.getNodeInfo();
+            log(JSON.stringify(info, undefined, 2));
+          })
+        })
     })
 
     .command({
@@ -47,16 +100,63 @@ export const PaymentModule = ({ paymentManager }) => ({
         .option('interactive', { hidden: true, default: true })
 
         .command({
-          command: ['setup'],
-          describe: 'Setup payment channel with counterparty.',
+          command: ['list'],
+          describe: 'List payment channels.',
+          builder: yargs => yargs
+            .option('interactive', { hidden: true, default: true }),
+
+          handler: asyncHandler(async () => {
+            const channels = await paymentManager.listChannels();
+            log(JSON.stringify(channels, undefined, 2));
+          })
+        })
+
+        .command({
+          command: ['get [channel]'],
+          describe: 'Get payment channel info.',
           builder: yargs => yargs
             .option('interactive', { hidden: true, default: true })
-            .option('counterparty-id', { alias: 'id', type: 'string' }),
+            .option('channel', { type: 'string' }),
 
           handler: asyncHandler(async (argv) => {
-            const { id } = argv;
-            const channel = await paymentManager.setupChannel(id);
+            const { channel } = argv;
+
+            const info = await paymentManager.getChannelInfo(channel);
+            log(JSON.stringify(info, undefined, 2));
+          })
+        })
+
+        .command({
+          command: ['setup [counterparty]'],
+          describe: 'Setup payment channel with counterparty.',
+          builder: yargs => yargs
+            .option('interactive', { hidden: true, default: true }),
+
+          handler: asyncHandler(async (argv) => {
+            const { counterparty } = argv;
+
+            assert(counterparty, 'Invalid counterparty ID.');
+
+            const channel = await paymentManager.setupChannel(counterparty);
             log(JSON.stringify(channel, undefined, 2));
+          })
+        })
+
+        .command({
+          command: ['deposit [channel] [amount]'],
+          describe: 'Deposit funds into the channel (initiator only).',
+          builder: yargs => yargs
+            .option('interactive', { hidden: true, default: true })
+            .option('channel', { type: 'string' })
+            .option('amount', { type: 'string' }),
+
+          handler: asyncHandler(async (argv) => {
+            const { channel, amount } = argv;
+
+            assert(channel, 'Invalid channel address.');
+            assert(amount, 'Invalid amount.');
+
+            await paymentManager.sendDepositTx(channel, amount);
           })
         })
     })
--- packages/cli-payment/src/payment-manager.js
+++ packages/cli-payment/src/payment-manager.js
@@ -4,6 +4,7 @@
 
 import assert from 'assert';
 import pino from 'pino';
+import { Wallet, utils, providers } from 'ethers';
 import { RestServerNodeService } from '@connext/vector-utils';
 
 const DEFAULT_TIMEOUT = '360000';
@@ -26,10 +27,91 @@ export class PaymentManager {
     this._connected = false;
   }
 
-  async getInfo () {
+  async getId () {
     await this._connect();
 
-    return { publicIdentifier: this._service.publicIdentifier };
+    return { id: this._service.publicIdentifier };
+  }
+
+  async getNodeInfo () {
+    const { provider } = this._config.get('services.payment');
+
+    assert(provider, 'Invalid payment provider endpoint.');
+
+    await this._connect();
+
+    const rpcProvider = new providers.JsonRpcProvider(provider);
+    const balance = await rpcProvider.getBalance(this._service.signerAddress);
+
+    return {
+      id: this._service.publicIdentifier,
+      address: this._service.signerAddress,
+      balance: utils.formatEther(balance)
+    };
+  }
+
+  async getWalletAddress () {
+    const { mnemonic, provider } = this._config.get('services.payment');
+
+    assert(mnemonic, 'Invalid account mnemonic.');
+    assert(provider, 'Invalid payment provider endpoint.');
+
+    const rpcProvider = new providers.JsonRpcProvider(provider);
+    const wallet = Wallet.fromMnemonic(mnemonic).connect(rpcProvider);
+
+    return wallet.address;
+  }
+
+  async getWalletBalance () {
+    const { mnemonic, provider } = this._config.get('services.payment');
+
+    assert(mnemonic, 'Invalid account mnemonic.');
+    assert(provider, 'Invalid payment provider endpoint.');
+
+    const rpcProvider = new providers.JsonRpcProvider(provider);
+    const wallet = Wallet.fromMnemonic(mnemonic).connect(rpcProvider);
+
+    const balance = await rpcProvider.getBalance(wallet.address);
+
+    return utils.formatEther(balance);
+  }
+
+  async sendFunds (address, amount) {
+    const { mnemonic, provider } = this._config.get('services.payment');
+
+    assert(mnemonic, 'Invalid account mnemonic.');
+    assert(provider, 'Invalid payment provider endpoint.');
+
+    const rpcProvider = new providers.JsonRpcProvider(provider);
+    const wallet = Wallet.fromMnemonic(mnemonic).connect(rpcProvider);
+
+    const tx = await wallet.sendTransaction({ to: address, value: utils.parseEther(amount) });
+    return tx.wait();
+  }
+
+  async listChannels () {
+    await this._connect();
+
+    const channelsResult = await this._service.getStateChannels({ publicIdentifier: this._service.publicIdentifier });
+    if (channelsResult.isError) {
+      throw channelsResult.getError();
+    }
+
+    return channelsResult.getValue();
+  }
+
+  async getChannelInfo (channelAddress) {
+    assert(channelAddress, 'Invalid channel.');
+
+    await this._connect();
+    const channelResult = await this._service.getStateChannel({ channelAddress, publicIdentifier: this._service.publicIdentifier });
+    if (channelResult.isError) {
+      throw channelResult.getError();
+    }
+
+    const channel = channelResult.getValue();
+
+    return channel;
   }
 
   async setupChannel (counterpartyIdentifier) {
@@ -53,6 +135,48 @@ export class PaymentManager {
     const { channelAddress } = result.getValue();
 
     return { channelAddress };
+  }
+
+  async sendDepositTx (channelAddress, amount) {
+    assert(channelAddress, 'Invalid channel.');
+    assert(amount, 'Invalid amount.');
+
+    const { assetId, provider } = this._config.get('services.payment');
+
+    assert(assetId, 'Invalid asset ID.');
+    assert(provider, 'Invalid payment provider endpoint.');
+
+    await this._connect();
+    const channelResult = await this._service.getStateChannel({ channelAddress, publicIdentifier: this._service.publicIdentifier });
+    if (channelResult.isError) {
+      throw channelResult.getError();
+    }
+
+    const channel = channelResult.getValue();
+    const depositAmt = utils.parseEther(amount);
+
+    console.log({
+      chainId: channel.networkContext.chainId,
+      amount: depositAmt.toString(),
+      assetId,
+      channelAddress: channel.channelAddress,
+      publicIdentifier: this._service.publicIdentifier
+    });
+
+    const depositTxRes = await this._service.sendDepositTx({
+      chainId: channel.networkContext.chainId,
+      amount: depositAmt.toString(),
+      assetId,
+      channelAddress: channel.channelAddress,
+      publicIdentifier: this._service.publicIdentifier
+    });
+
+    if (depositTxRes.isError) {
+      throw depositTxRes.getError();
+    }
+
+    const rpcProvider = new providers.JsonRpcProvider(provider);
+    await rpcProvider.waitForTransaction(depositTxRes.getValue().txHash);
   }
 
   async _connect () {
