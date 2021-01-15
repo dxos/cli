1e031920b6045c02b22ac6ae32cc4b7edfd5fac3
Egor Gripasov <gripasovegor@gmail.com>
2021-01-15 13:49:41 +0300

    add template downloader.

---
+++ packages/cli-core/src/util/template.js
@@ -0,0 +1,134 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+import { pathExists, copy, remove } from 'fs-extra';
+import download from 'download';
+import os from 'os';
+import path from 'path';
+import URL from 'url';
+
+const GITHUB_HOSTNAME = 'github.com';
+const CONFIG = { timeout: 30000, extract: true, strip: 1, mode: '755' };
+
+/**
+ * Template Helper.
+ */
+export class TemplateHelper {
+  /**
+   * @param {Object} url
+   * @returns {Object}
+   */
+  static parseGitHubURL (url) {
+    const parts = url.pathname.split('/');
+    const isSubdir = parts.length > 4;
+    const owner = parts[1];
+    const repo = parts[2];
+    const branch = isSubdir ? parts[4] : 'master';
+
+    // Validate if given url is a valid GitHub url.
+    if (url.hostname !== 'github.com' || !owner || !repo) {
+      throw new Error(`The URL must be a valid GitHub URL, e.g.:
+        https://github.com/wirelineio/service-template`);
+    }
+
+    let pathToDir = '';
+    for (let i = 5; i <= parts.length - 1; i++) {
+      pathToDir = path.join(pathToDir, parts[i]);
+    }
+
+    const downloadUrl = ['https://github.com/', owner, '/', repo, '/archive/', branch, '.zip'].join('');
+
+    return {
+      owner,
+      repo,
+      branch,
+      downloadUrl,
+      isSubdir,
+      pathToDir
+    };
+  }
+
+  /**
+   * Parse URL and call the appropriate adaptor.
+   *
+   * @param {string} inputUrl
+   * @throws {Error}
+   * @returns {Object}
+   */
+  static parseURL (inputUrl) {
+    console.assert(inputUrl, 'URL is required');
+
+    // eslint-disable-next-line
+    const url = URL.parse(inputUrl.replace(/\/$/, ''));
+
+    if (!url.host) {
+      throw new Error('The URL you passed is not valid');
+    }
+
+    switch (url.hostname) {
+      case GITHUB_HOSTNAME: {
+        return TemplateHelper.parseGitHubURL(url);
+      }
+      default: {
+        throw new Error('The URL you passed is not one of the valid providers.');
+      }
+    }
+  }
+
+  /**
+   * Downloads template from repo.
+   * @param {string} inputUrl
+   * @param {string} [downloadPath]
+   * @param {string} githubToken
+   * @param {boolean} force
+   * @returns {Promise}
+   */
+  static async downloadTemplateFromRepo (inputUrl, githubToken, downloadPath, force) {
+    const repoInfo = TemplateHelper.parseURL(inputUrl);
+
+    // If template is in a subdirectory of a repo, use directory name as default build directory.
+    if (repoInfo.pathToDir && repoInfo.isSubdir) {
+      const basePath = path.basename(repoInfo.pathToDir);
+      downloadPath || (downloadPath = `./${basePath}`); // if no path is specified, use the ./{basePath}
+    } else {
+      downloadPath || (downloadPath = `./${repoInfo.repo}`); // if no path is specified, use the ./{repo-name}
+    }
+
+    let downloadServicePath;
+
+    if (repoInfo.isSubdir) {
+      downloadServicePath = path.join(os.tmpdir(), repoInfo.repo);
+    } else if (path.isAbsolute(downloadPath)) {
+      downloadServicePath = downloadPath;
+    } else {
+      downloadServicePath = path.join(process.cwd(), downloadPath);
+    }
+
+    let servicePath;
+
+    if (path.isAbsolute(downloadPath)) {
+      servicePath = downloadPath;
+    } else {
+      servicePath = path.join(process.cwd(), downloadPath);
+    }
+
+    if ((await pathExists(servicePath)) && path.resolve(process.cwd(), downloadPath) !== process.cwd() && !force) {
+      throw new Error('Folder already exists!');
+    }
+
+    await download(
+      repoInfo.downloadUrl,
+      downloadServicePath,
+      githubToken ? { headers: { Authorization: `token ${githubToken}` }, ...CONFIG } : CONFIG
+    );
+
+    if (repoInfo.isSubdir) {
+      const directory = path.join(downloadServicePath, repoInfo.pathToDir);
+      await copy(directory, servicePath);
+      await remove(downloadServicePath);
+    }
+
+    return servicePath;
+  }
+}
---
+++ packages/cli-core/src/util/template.test.js
@@ -0,0 +1,30 @@
+import crypto from 'crypto';
+import { tmpdir } from 'os';
+import path from 'path';
+import fs from 'fs';
+import { TemplateHelper } from './template';
+
+const DEFAULT_TEMPLATE = 'https://github.com/wirelineio/logbot-template';
+
+const tmpFolder = () => {
+  return path.join(tmpdir(), `random.${crypto.randomBytes(6).readUIntLE(0, 6).toString(36)}`);
+};
+
+const appPath = tmpFolder();
+
+describe('downloadTemplateFromRepo', () => {
+  test('it should download template', async () => {
+    const result = await TemplateHelper.downloadTemplateFromRepo(DEFAULT_TEMPLATE, null, appPath, false);
+    const folderLength = fs.readdirSync(appPath).length;
+    expect(result).toBe(appPath);
+    expect(folderLength).toBeGreaterThan(0);
+  });
+
+  test('it should throw the error that folder already exists', async () => {
+    await expect(TemplateHelper.downloadTemplateFromRepo(DEFAULT_TEMPLATE, null, appPath, false)).rejects.toThrow('Folder already exists');
+  });
+
+  test('it should recreate that folder', async () => {
+    await expect(TemplateHelper.downloadTemplateFromRepo(DEFAULT_TEMPLATE, null, appPath, true)).resolves.toBeDefined();
+  });
+});
