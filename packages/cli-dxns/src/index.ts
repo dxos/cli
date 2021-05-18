b5728fa2de69b5d37b6f61ef889d4b41ce81834d
egorgripasov <gripasovegor@gmail.com>
2021-05-18 16:56:10 -0400

    add ts to cli-dxns

--- packages/cli-dxns/src/index.ts
+++ packages/cli-dxns/src/index.ts
@@ -2,17 +2,17 @@
 // Copyright 2020 DXOS.org
 //
 
+import path from 'path';
+import { readFileSync } from 'fs';
+
 import { createCLI } from '@dxos/cli-core';
 
 import { DXNSModule } from './modules/dxns';
-
-import info from '../extension.yml';
-import compose from '../docker-compose.yml';
 
 module.exports = createCLI({
   modules: [DXNSModule],
   dir: __dirname,
   main: !module.parent,
-  info,
-  compose
+  info: readFileSync(path.join(__dirname, './extension.yml')).toString(),
+  compose: readFileSync(path.join(__dirname, './docker-compose.yml')).toString()
 });
