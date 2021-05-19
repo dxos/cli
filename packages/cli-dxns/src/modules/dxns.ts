026a0b2c6f1551e8fc7b659e20b9033612901f50
egorgripasov <gripasovegor@gmail.com>
2021-05-19 11:50:46 -0400

    cleanup

--- packages/cli-dxns/src/modules/dxns.ts
+++ packages/cli-dxns/src/modules/dxns.ts
@@ -2,12 +2,14 @@
 // Copyright 2020 DXOS.org
 //
 
+import { Argv } from 'yargs';
+
 import { asyncHandler } from '@dxos/cli-core';
 
 export const DXNSModule = () => ({
   command: ['dxns'],
   describe: 'DXNS operations.',
-  builder: yargs => yargs
+  builder: (yargs: Argv) => yargs
     .command({
       command: ['test'],
       describe: 'Test DXNS command.',
