f1cf33c6fd25be48c77c9c61d087e1d3da1c6b27
egorgripasov <gripasovegor@gmail.com>
2021-05-26 14:07:57 -0400

    Add schema & resource && record resolvers.

--- packages/cli-dxns/src/modules/dxns.ts
+++ packages/cli-dxns/src/modules/dxns.ts
@@ -4,7 +4,11 @@
 
 import { Argv } from 'yargs';
 
-import { asyncHandler, print } from '@dxos/cli-core';
+import { asyncHandler } from '@dxos/cli-core';
+
+import { listRecords, getRecords, addRecord } from '../handlers/record';
+import { listResources } from '../handlers/resource';
+import { listSchemas, querySchema, getSchema, addSchema } from '../handlers/schema';
 
 interface Params {
   config: any,
@@ -18,22 +22,90 @@ export const DXNSModule = (params: Params) => {
     describe: 'DXNS operations.',
     builder: (yargs: Argv) => yargs
       .command({
-        command: ['test'],
-        describe: 'Test DXNS command.',
+        command: ['schema'],
+        describe: 'Schema commands.',
+        handler: () => {},
+        builder: yargs => yargs
+          .command({
+            command: ['list'],
+            describe: 'List schemas.',
+            handler: asyncHandler(listSchemas({ getDXNSClient }))
+          })
 
-        handler: asyncHandler(async (argv: any) => {
-          const { json } = argv;
+          .command({
+            command: ['query <dxn>'],
+            describe: 'Query schema by dxn.',
+            builder: yargs => yargs
+              .option('dxn', { type: 'string' }),
 
-          const client = await getDXNSClient();
+            handler: asyncHandler(querySchema({ getDXNSClient }))
+          })
 
-          const domains = await client.registryApi.getDomains();
+          .command({
+            command: ['get <cid>'],
+            describe: 'Get schema by cid.',
+            builder: yargs => yargs
+              .option('cid', { type: 'string' }),
 
-          print(domains.map((domain: any) => ({
-            domainKey: domain.domainKey.toHex(),
-            name: domain.name,
-            owners: domain.owners
-          })), { json });
-        })
+            handler: asyncHandler(getSchema({ getDXNSClient }))
+          })
+
+          .command({
+            command: ['add <path>'],
+            describe: 'Add new schema.',
+            builder: yargs => yargs
+              .option('name', { describe: 'Name of the record.', type: 'string' })
+              .option('domain', { describe: 'Domain key for the record.', type: 'string' })
+              .option('path', { type: 'string' }),
+
+            handler: asyncHandler(addSchema({ getDXNSClient }))
+          })
+      })
+
+      .command({
+        command: ['record'],
+        describe: 'Record commands.',
+        handler: () => {},
+        builder: yargs => yargs
+          .command({
+            command: ['list'],
+            describe: 'List records.',
+            handler: asyncHandler(listRecords({ getDXNSClient }))
+          })
+
+          .command({
+            command: ['get <id>'],
+            describe: 'Get record.',
+            builder: yargs => yargs
+              .option('id', { type: 'string' }),
+
+            handler: asyncHandler(getRecords({ getDXNSClient }))
+          })
+
+          .command({
+            command: ['add'],
+            describe: 'Add records',
+            builder: yargs => yargs
+              .option('data', { required: true, describe: 'Data encoded in json.', type: 'string' })
+              .option('schema', { required: true, describe: 'Schema cid', type: 'string' })
+              .option('fqn', { required: true, describe: 'Message fully-qualified name inside the referenced schema.', type: 'string' })
+              .option('name', { describe: 'Register a resource name for this record.', type: 'string' })
+              .option('domain', { describe: 'Specify a domain key for the record.', type: 'string' }),
+
+            handler: asyncHandler(addRecord({ getDXNSClient }))
+          })
+      })
+
+      .command({
+        command: ['resource'],
+        describe: 'Resource commands.',
+        handler: () => {},
+        builder: yargs => yargs
+          .command({
+            command: ['list'],
+            describe: 'List resources.',
+            handler: asyncHandler(listResources({ getDXNSClient }))
+          })
       })
   };
 };
