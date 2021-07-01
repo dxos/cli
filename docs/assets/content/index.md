d30c5bb804affbe76d5f0696df68bad43fe39799
Zarco Nontol <zarconontol@gmail.com>
2021-07-01 19:08:59 -0300

    feat(docs): adding docs to project

---
+++ docs/assets/content/index.md
@@ -0,0 +1,6 @@
+---
+title: Home
+description: Exploring the DXOS platform
+---
+
+Welcome to the DXOS documentation site. Let's build it out together!
---
+++ docs/gatsby-config.js
@@ -0,0 +1,40 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+const themeOptions = require("@dxos/docs-theme/theme-options");
+
+module.exports = {
+  pathPrefix: "/cli",
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/assets/images`
+      }
+    },
+
+    // Image processing
+    // https://www.gatsbyjs.org/packages/gatsby-plugin-sharp
+    // https://www.gatsbyjs.org/packages/gatsby-transformer-sharp
+    // https://github.com/lovell/sharp
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+
+    {
+      resolve: "gatsby-theme-apollo-docs",
+      options: {
+        ...themeOptions,
+        contentDir: "assets/content",
+        root: __dirname,
+        githubRepo: "dxos/cli",
+        description: "DXOS - The Decentralized Operating System",
+        subtitle: "DXOS cli",
+        sidebarCategories: {
+          null: ["index", "cli"]
+        }
+      }
+    }
+  ]
+};
