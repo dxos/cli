6bfdc0db03102069649c5bd98c1a9259f3b2c1d8
rzadp <roopert7@gmail.com>
2021-09-01 16:58:01 +0200

    First working test

--- packages/cli-data/src/modules/party.test.ts
+++ packages/cli-data/src/modules/party.test.ts
@@ -2,8 +2,26 @@
 // Copyright 2020 DXOS.org
 //
 
-describe('cli-data: Party', () => {
-  test('Creates a party.', () => {
+import { Client } from '@dxos/client';
+import { createKeyPair } from '@dxos/crypto';
 
+import { StateManager } from '../state-manager';
+import { createCommand } from './commands/create';
+
+const getReadlineInterface = () => {
+  throw new Error('getReadlineInterface not mocked.');
+};
+
+describe('cli-data: Party', () => {
+  test('Creates a party.', async () => {
+    const client = new Client();
+    await client.initialize();
+    await client.halo.createProfile({ ...createKeyPair(), username: 'Test' });
+    const stateManager = new StateManager(() => client, getReadlineInterface, {});
+    expect(await stateManager.getParty()).toBeNull();
+
+    await createCommand(stateManager);
+
+    expect(await stateManager.getParty()).toBeDefined();
   });
 });
