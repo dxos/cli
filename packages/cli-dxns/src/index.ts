634c52c3b86c2edfbb2dbb9f1baab7fe1d85dee6
rzadp <roopert7@gmail.com>
2021-10-04 15:56:49 +0200

    Update usage

--- packages/cli-dxns/src/index.ts
+++ packages/cli-dxns/src/index.ts
@@ -9,7 +9,7 @@ import { readFileSync } from 'fs';
 import path from 'path';
 
 import { createCLI } from '@dxos/cli-core';
-import { createApiPromise, IAuctionsClient, IRegistryClient, ApiTransactionHandler, createKeyring, RegistryClient, AuctionsApi } from '@dxos/registry-client';
+import { createApiPromise, IAuctionsClient, IRegistryClient, ApiTransactionHandler, createKeyring, RegistryClient, AuctionsClient } from '@dxos/registry-client';
 
 import { DXNSModule } from './modules/dxns';
 
@@ -43,7 +43,7 @@ const _createClient = async (config: any, options: any): Promise<DXNSClient | un
     const apiPromise = await createApiPromise(apiServerUri);
 
     const registryClient = new RegistryClient(apiPromise, keypair);
-    const auctionsClient = new AuctionsApi(apiPromise, keypair);
+    const auctionsClient = new AuctionsClient(apiPromise, keypair);
     const transactionHandler = new ApiTransactionHandler(apiPromise, keypair);
 
     return {
