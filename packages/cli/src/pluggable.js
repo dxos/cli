550964dc6e035b2161a798728ddd61d52a617406
egorgripasov <gripasovegor@gmail.com>
2021-11-08 19:32:06 -0500

    fix: allow to develop external cli modules.

--- packages/cli/src/pluggable.js
+++ packages/cli/src/pluggable.js
@@ -87,6 +87,7 @@ export class Pluggable {
     this._version = version;
     this._workspaceRoot = getWorkspaceRoot(__dirname);
     this._isInWorkspace = this._workspaceRoot && fs.existsSync(path.resolve(this._workspaceRoot, 'node_modules', this._moduleName));
+    this._isInCWD = fs.existsSync(path.join(process.cwd(), 'package.json')) && require(path.join(process.cwd(), 'package.json'))?.name === this._moduleName;
     this._installed = this.isInstalled();
   }
 
@@ -112,8 +113,7 @@ export class Pluggable {
 
   get modulePath () {
     if (!this._modulePath) {
-      const { moduleName } = this;
-      const pkgPath = require.resolve(`${moduleName}/package.json`);
+      const pkgPath = require.resolve(this._isInCWD ? path.join(process.cwd(), 'package.json') : `${this.moduleName}/package.json`);
       const pkg = require(pkgPath);
       this._modulePath = path.resolve(path.dirname(pkgPath), pkg.main);
     }
@@ -138,7 +138,7 @@ export class Pluggable {
    */
   isInstalled () {
     const { moduleName } = this;
-    if (this._isInWorkspace) {
+    if (this._isInWorkspace || this._isInCWD) {
       return true;
     }
     try {
