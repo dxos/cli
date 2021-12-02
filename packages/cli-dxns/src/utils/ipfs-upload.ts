4320762b0145da87b78602327316a4a4b49d5894
egorgripasov <gripasovegor@gmail.com>
2021-12-02 14:53:15 -0500

    publish command.

--- packages/cli-dxns/src/utils/ipfs-upload.ts
+++ packages/cli-dxns/src/utils/ipfs-upload.ts
@@ -7,19 +7,28 @@ import fs from 'fs';
 import IpfsHttpClient from 'ipfs-http-client';
 const { globSource } = IpfsHttpClient as any;
 
-export const uploadToIPFS = async (config: any, path: string): Promise<string> => {
+interface UploadOptions {
+  timeout: string | number
+  progress?: Function
+};
+
+export const uploadToIPFS = async (config: any, path: string, options?: UploadOptions): Promise<string> => {
+  const { timeout, progress } = options || {};
+
   const ipfsServer = config.get('services.ipfs.server');
   assert(ipfsServer, 'Invalid IPFS Server.');
+
   const ipfsClient = IpfsHttpClient({
     url: ipfsServer,
-    timeout: '1m'
+    timeout: timeout || '1m'
   });
+
   if (!fs.existsSync(path)) {
     throw new Error('Incorrect path to definitons. File or directory does not exist');
   }
   if (fs.lstatSync(path).isDirectory()) {
     const source = globSource(path, { recursive: true });
-    const addResult = await ipfsClient.add(source);
+    const addResult = await ipfsClient.add(source, { progress });
     return addResult.cid.toString();
   } else {
     const content = fs.readFileSync(path);
