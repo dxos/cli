b15278dd3240c954655af2fbc818412bd434369f
rzadp <roopert7@gmail.com>
2021-12-10 17:33:26 +0100

    Revert "fix: Revert Rushification (#358)"
    dffac353dfe54f0d96980824189618fb9f913db3

--- packages/cli-core/src/util/qrcode.test.ts
+++ packages/cli-core/src/util/qrcode.test.ts
@@ -2,10 +2,9 @@
 // Copyright 2021 DXOS.org
 //
 
-import expect from 'expect';
 import qrcode from 'qrcode-terminal';
 
-it('generates a QR code', () => {
+test('generates a QR code', () => {
   qrcode.generate('https://dxos.org', { small: true }, (code: string) => {
     expect(code).not.toBeNull();
     console.log(code);
