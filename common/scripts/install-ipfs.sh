9e3f3361a418039213ffcb5e66df46036921d085
yivlad <jacenko.vlad@gmail.com>
2022-01-18 19:28:33 +0100

    change dir

--- common/scripts/install-ipfs.sh
+++ common/scripts/install-ipfs.sh
@@ -8,6 +8,6 @@ set -e
 
 wget https://dist.ipfs.io/go-ipfs/v0.11.0/go-ipfs_v0.11.0_linux-amd64.tar.gz
 tar -xvzf go-ipfs_v0.11.0_linux-amd64.tar.gz
-pushd ~/go-ipfs
+pushd go-ipfs
 sudo bash install.sh
 popd
