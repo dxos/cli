1d651ade1c066aef895fa91d0fb8668125d741cb
yivlad <jacenko.vlad@gmail.com>
2022-01-18 19:21:47 +0100

    Add install ipfs script

---
+++ common/scripts/install-ipfs.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+set -e
+
+wget https://dist.ipfs.io/go-ipfs/v0.11.0/go-ipfs_v0.11.0_linux-amd64.tar.gz
+tar -xvzf go-ipfs_v0.11.0_linux-amd64.tar.gz
+pushd go-ipfs
+sudo bash install.sh
+popd
