e3bb78c30924298c81dcc0e50a913cbf0ec83819
yivlad <jacenko.vlad@gmail.com>
2022-01-18 17:10:17 +0100

    Revert "chore: Bot cli revive spawn command"
    This reverts commit fef91ae5034c722bc637053d85d3b2c937f48611.

--- packages/cli-bot/src/modules/bot.ts
+++ packages/cli-bot/src/modules/bot.ts
@@ -4,24 +4,21 @@
 
 import { Argv } from 'yargs';
 
-import { asyncHandler, CoreState } from '@dxos/cli-core';
-import type { StateManager } from '@dxos/cli-data';
+import { asyncHandler } from '@dxos/cli-core';
 import type { DXNSClient } from '@dxos/cli-dxns';
 
-import { publish, query, register, build, botBuildOptions, botRegisterOptions, botSpawnOptions, spawn } from '../handlers/bot';
+import { publish, query, register, build, botBuildOptions, botRegisterOptions } from '../handlers/bot';
 import { botFactoryStartOptions, botFactoryInstallOptions, install, setup, start } from '../handlers/bot-factory';
 
 export interface Params {
   config: any,
-  getDXNSClient(): Promise<DXNSClient>,
-  stateManager: StateManager,
-  cliState: CoreState['cliState']
+  getDXNSClient(): Promise<DXNSClient>
 }
 
 /**
  * Bot CLI module.
  */
-export const BotModule = ({ config, getDXNSClient, cliState, stateManager }: Params) => {
+export const BotModule = ({ config, getDXNSClient }: Params) => {
   return {
     command: ['bot'],
     describe: 'Bot CLI.',
@@ -58,13 +55,21 @@ export const BotModule = ({ config, getDXNSClient, cliState, stateManager }: Par
           })
       })
 
-      .command({
-        command: ['spawn'],
-        describe: 'Spawn new bot instance.',
-        builder: botSpawnOptions,
+    // .command({
+    //   command: ['spawn'],
+    //   describe: 'Spawn new bot instance.',
+    //   builder: yargs => yargs
+    //     .option('topic', { alias: 't', type: 'string' })
+    //     .option('env', { type: 'string' })
+    //     .option('ipfsCID', { type: 'string' })
+    //     .option('ipfsEndpoint', { type: 'string' })
+    //     .option('id', { type: 'string' })
+    //     .option('name', { type: 'string' })
+    //     .option('bot-name', { type: 'string' })
+    //     .option('bot-path', { type: 'string' }),
 
-        handler: asyncHandler(spawn({ cliState, stateManager, config }))
-      })
+    //   handler: asyncHandler(spawn({ cliState, stateManager }))
+    // })
 
     // .command({
     //   command: ['invite'],
