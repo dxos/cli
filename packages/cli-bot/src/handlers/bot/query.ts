22712d5b6d7c0d4540637633f3d436f1a4227053
yivlad <jacenko.vlad@gmail.com>
2022-02-17 13:09:10 +0100

    chore: Update bot query

--- packages/cli-bot/src/handlers/bot/query.ts
+++ packages/cli-bot/src/handlers/bot/query.ts
@@ -6,23 +6,53 @@ import { Arguments } from 'yargs';
 
 import { print, CoreOptions } from '@dxos/cli-core';
 import { raise } from '@dxos/debug';
-import { CID, DXN, RegistryDataRecord } from '@dxos/registry-client';
+import { DXN, RegistryDataRecord, Resource } from '@dxos/registry-client';
 
 import type { Params } from '../../modules/bot';
 
 const BOT_TYPE_DXN = 'dxos:type.bot';
 
-export const displayBots = (record: RegistryDataRecord) => {
-  return ({
-    cid: record.cid.toString(),
-    created: record.meta.created,
-    description: record.meta.description,
-    hash: CID.from(Buffer.from(record.data.hash, 'base64')).toString()
-  });
-};
+interface BotData {
+  dxn: DXN,
+  tag?: string,
+  version?: string,
+  description?: string,
+  created?: string
+}
 
 interface QueryParams {
   getDXNSClient: Params['getDXNSClient']
+}
+
+export const mergeResourceRecords = (records: RegistryDataRecord[], resources: Resource[]) => {
+  const bots: BotData[] = [];
+  for (const resource of resources) {
+    for (const tag of Object.keys(resource.tags)) {
+      for(const record of records) {
+        if (resource.tags[tag]?.equals(record.cid)) {
+          bots.push({
+            dxn: resource.id,
+            tag,
+            description: record.meta.description,
+            created: record.meta.created?.toISOString()
+          });
+        }
+      }
+    }
+    for (const version of Object.keys(resource.versions)) {
+      for(const record of records) {
+        if (resource.versions[version]?.equals(record.cid)) {
+          bots.push({
+            dxn: resource.id,
+            version,
+            description: record.meta.description,
+            created: record.meta.created?.toISOString()
+          });
+        }
+      }
+    }
+  }
+  return bots;
 }
 
 export const query = ({ getDXNSClient }: QueryParams) => async (argv: Arguments<CoreOptions>) => {
@@ -31,8 +61,9 @@ export const query = ({ getDXNSClient }: QueryParams) => async (argv: Arguments<
   const registry = client.registryClient;
   const botType = await registry.getResourceRecord(DXN.parse(BOT_TYPE_DXN), 'latest') ?? raise(new Error('Bot type not found.'));
   const records = await registry.getDataRecords({ type: botType.record.cid });
+  const resources = await registry.queryResources({ type: botType.record.cid });
 
-  const bots = records.map(displayBots);
+  const bots = mergeResourceRecords(records, resources);
 
   if (bots.length) {
     print(bots, { json });
