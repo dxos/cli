c4ddd272710d6a70214ae504822178e795fa1266
rzadp <roopert7@gmail.com>
2022-02-28 13:50:15 +0100

    ci: Update path

--- packages/e2e/src/e2e.test.ts
+++ packages/e2e/src/e2e.test.ts
@@ -75,7 +75,7 @@ describe('CLI', () => {
       }
 
       // TODO(rzadp): Change to main when this change gets merged.
-      await cmd(`profile init --name ${PROFILE_NAME} --template-url https://raw.githubusercontent.com/dxos/cli/rzadp/dxns-accounts/packages/cli/profiles/e2e.yml`).run();
+      await cmd(`profile init --name ${PROFILE_NAME} --template-url https://raw.githubusercontent.com/dxos/cli/main/packages/cli/profiles/e2e.yml`).run();
     });
 
     it('select profile', async () => {
