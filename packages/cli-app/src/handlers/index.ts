7174f48f69cdbe98aaa60f748cc18244335c32a2
egorgripasov <gripasovegor@gmail.com>
2022-03-08 12:59:35 -0500

    feat: rm app deploy; cleanup.

--- packages/cli-app/src/handlers/index.ts
+++ packages/cli-app/src/handlers/index.ts
@@ -2,10 +2,7 @@
 // Copyright 2020 DXOS.org
 //
 
-export * from './build';
 export * from './create';
 export * from './config';
-export * from './publish';
 export * from './query';
-export * from './register';
 export { default as serve } from './serve';
--- packages/cli-app/src/shims.d.ts
+++ packages/cli-app/src/shims.d.ts
@@ -2,6 +2,5 @@
 // Copyright 2021 DXOS.org
 //
 
-declare module 'semver/functions/inc';
 declare module 'lodash-clean';
 declare module 'node-fetch';
