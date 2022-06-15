6b745145af30121b808b84c8f53bce998932c2a5
Josiah Witt <josiah@witt.life>
2022-06-15 14:47:45 -0400

    chore: Remove debug

--- packages/cli-dxns/src/handlers/deploy/register.ts
+++ packages/cli-dxns/src/handlers/deploy/register.ts
@@ -119,8 +119,6 @@ const registerVersion = async (
 ) => {
   const resource = await registry.getResource(name);
 
-  console.log({ resource, tags: resource?.tags, tag: resource?.tags[version], name, version, recordCID, account, skipExisting });
-
   if (skipExisting && resource?.tags[version]) {
     return;
   }
