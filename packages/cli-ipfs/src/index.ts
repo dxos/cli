d4cdb7a4982f842842a8fc8f085ce84965827f01
egorgripasov <gripasovegor@gmail.com>
2022-06-27 11:40:14 -0400

    fix: add ipfs to services list.

--- packages/cli-ipfs/src/index.ts
+++ packages/cli-ipfs/src/index.ts
@@ -12,5 +12,6 @@ module.exports = createCLI({
   dir: __dirname,
   main: !module.parent,
   modules: [IPFSModule],
-  info: loadYml(path.join(__dirname, `../${EXTENSION_CONFIG_FILENAME}`))
+  info: loadYml(path.join(__dirname, `../${EXTENSION_CONFIG_FILENAME}`)),
+  docker: loadYml(path.join(__dirname, '../docker-compose.yml'))
 });
